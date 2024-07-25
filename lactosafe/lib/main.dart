acfa68acb5498ca1f0bb3f0930736da9733f0e1e
João Vitor da Silva Pires <jvitorpires1999@gmail.com>
2024-07-25 19:10:19 -0300

    feat--MaterialApp-Colors-from-theme-fix

--- lactosafe/lib/main.dart
+++ lactosafe/lib/main.dart
@@ -34,29 +34,15 @@ class MyApp extends StatelessWidget {
     AppSettings.screenWidth = MediaQuery.of(context).size.width;
     AppSettings.screenHeight = MediaQuery.of(context).size.height;
 
-    MaterialColor appPrimarySwatch =
-        MaterialColor(AppColors.orange.value, <int, Color>{
-      50: AppColors.orange.withOpacity(0.1),
-      100: AppColors.orange.withOpacity(0.2),
-      200: AppColors.orange.withOpacity(0.3),
-      300: AppColors.orange.withOpacity(0.4),
-      400: AppColors.orange.withOpacity(0.5),
-      500: AppColors.orange.withOpacity(0.6),
-      600: AppColors.orange.withOpacity(0.7),
-      700: AppColors.orange.withOpacity(0.8),
-      800: AppColors.orange.withOpacity(0.9),
-      900: AppColors.orange.withOpacity(1.0),
-    } //Criando novo primary swatch
-            );
-
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'LactoSafe',
         theme: ThemeData(
-          primarySwatch: appPrimarySwatch,
-          appBarTheme: AppBarTheme(
-              elevation: 0, backgroundColor: AppColors.backgroundColor),
-        ),
+            primaryColor: AppColors.orange,
+            appBarTheme: AppBarTheme(
+                elevation: 0, backgroundColor: AppColors.backgroundColor),
+            colorScheme: ColorScheme.fromSwatch().copyWith(
+                primary: AppColors.orange, secondary: AppColors.orange)),
         initialRoute: WelcomePage.routeName,
         routes: {
           SignIn.routeName: (context) => const SignIn(),
